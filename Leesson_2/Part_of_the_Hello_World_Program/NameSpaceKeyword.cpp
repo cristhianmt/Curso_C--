38cb1f332133cedffce7ed14a30ccfe3b8c4ee3e
Cristhian <cmartinezg1706@alumno.ipn.mx>
2022-10-18 18:18:30 -0500

    using namespace

---
+++ Leesson_2/Part_of_the_Hello_World_Program/NameSpaceKeyword.cpp
@@ -0,0 +1,18 @@
+/*
+Another demonstration of the using Keyword using namespace
+*/
+
+// Pre-processor directive
+#include <iostream>
+
+// Start of your program
+int main(){
+    using std::cout;
+    using std::endl;
+
+    // Write to the screen using cout
+    cout << "Hello World!" << endl;
+
+    // Return a value to the OS
+    return 0;
+}
--- Leesson_2/Part_of_the_Hello_World_Program/namespace.cpp
+++ Leesson_2/Part_of_the_Hello_World_Program/namespace.cpp
@@ -1,3 +1,9 @@
+/*
+Many programmers find it tedious to repeatedly add the std namespace specifier to
+their code when using cout and other such features contained in the same. The using
+namespace declaration as demonstrated will help you avoid this repetition.
+*/
+
 // Pre-processor
 #include<iostream>
 
@@ -7,7 +13,7 @@ int main(){
     using namespace std;
 
     // Write to the screen using printf
-    printf("Hello World");
+    cout << "Hello World" << endl;
 
     // Return a value to the OS
     return 0;
