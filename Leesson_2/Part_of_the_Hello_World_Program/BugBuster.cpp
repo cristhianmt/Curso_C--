28c40077e9c2c105193cb4b48f72fc880c55fe69
Cristhian <cmartinezg1706@alumno.ipn.mx>
2022-10-18 18:25:37 -0500

    exercises

---
+++ Leesson_2/Part_of_the_Hello_World_Program/BugBuster.cpp
@@ -0,0 +1,14 @@
+/*
+Enter this program and compile it. Why does it fail?
+
+#include <iostream>
+void main(){
+    std::cout << "Is there a bug here" << endl;
+}
+*/
+
+
+#include <iostream>
+int main(){
+    std::cout << "Is there a bug here" << std::endl;
+}
