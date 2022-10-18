acf62087451b72da0a0ec0a6722a5df3a14fffda
Cristhian <cmartinezg1706@alumno.ipn.mx>
2022-10-18 15:35:57 -0500

    create name space

---
+++ Leesson_2/Part_of_the_Hello_World_Program/namespace.cpp
@@ -0,0 +1,14 @@
+// Pre-processor
+#include<iostream>
+
+// Start of your program 
+int main(){
+    // Tell the compiler what namespace to search in 
+    using namespace std;
+
+    // Write to the screen using printf
+    printf("Hello World");
+
+    // Return a value to the OS
+    return 0;
+}
